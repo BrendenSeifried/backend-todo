617e4da39b887859349568b1ea7e33ab66b352e6
BrendenSeifried <brendenseifried@gmail.com>
2022-06-27 19:22:35 -0700

    todos.js controller created and with it the first .post route

--- __tests__/todo.test.js
+++ __tests__/todo.test.js
@@ -2,6 +2,17 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserServices');
+
+const LoggedIn = async (props = {}) => {
+  const password = props.password ?? testUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...testUser, ...props });
+  const { email } = user;
+
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
+};
 
 describe('todo test suite', () => {
   beforeEach(() => {
