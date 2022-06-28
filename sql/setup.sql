d8371d2ec9c0de9f8ba8e4cf7dee47cbef31737d
BrendenSeifried <brendenseifried@gmail.com>
2022-06-27 19:01:31 -0700

    SQL table for todo created, beginning second virticle slice

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,9 +1,17 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS users cascade;
+DROP TABLE IF EXISTS todo;
 
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     email VARCHAR NOT NULL UNIQUE,
     password_hash VARCHAR NOT NULL
+);
+
+CREATE TABLE todo ( 
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    completed BOOLEAN NOT NULL DEFAULT(false), 
+    user_id INT NOT NULL
 );
